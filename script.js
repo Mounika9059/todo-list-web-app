516f83cd14ba993ca367f35cf6ed0eb4d991a17b
Songa Mounika <songamounika99@gmail.com>
2026-08-16 17:04:54 +0530

    Refactor task list appending method

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function addTask() {
     const input = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+
     const task = input.value.trim();
 
     if (task === "") {
+        alert("Please enter a task!");
         return;
     }
 
     const li = document.createElement("li");
     li.textContent = task;
 
-    document.getElementById("taskList").appendChild(li);
+    taskList.appendChild(li);
 
     input.value = "";
 }
